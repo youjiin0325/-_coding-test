3a8b80e19e73059debe75b9045f54779416be79f
Jiin Yu <93093603+youjiin0325@users.noreply.github.com>
2023-10-15 19:10:04 +0900

    [level unrated] Title: 약수 구하기, Time: 0.03 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120897. 약수 구하기/약수 구하기.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func solution(_ n:Int) -> [Int] {
+    var arr : [Int] = []
+    for i in 1...n{
+        if n % i == 0{
+            arr.append(i)
+        }
+
+    }
+    return arr
+}
