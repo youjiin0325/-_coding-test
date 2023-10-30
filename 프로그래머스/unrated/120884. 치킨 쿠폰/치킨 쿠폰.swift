49cd0913324937b12085ccd4a499deb215bc31f0
Jiin Yu <93093603+youjiin0325@users.noreply.github.com>
2023-10-30 22:41:18 +0900

    [level unrated] Title: 치킨 쿠폰, Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120884. 치킨 쿠폰/치킨 쿠폰.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ chicken:Int) -> Int {
+    var count = 0
+    var rest = chicken
+    while rest >= 10 {
+        count += rest / 10
+        rest = rest / 10 + rest % 10
+    }
+    return count
+}
